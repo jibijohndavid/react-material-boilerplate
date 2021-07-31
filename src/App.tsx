79465667b29636b3ac64daae35348ed6886809c5
Jibi John David <jibijohndavid@gmail.com>
2021-07-31 13:37:03 +0530

    ESLint Updates - Added lint extentions and plugins - Added necessary rules - Added pre-commit hook - Added package scripts - Removed css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <p>Material React Boilerplate</p>
+      <a
+        href="https://github.com/jibijohndavid/react-material-boilerplate"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        View Repo
+      </a>
+    </header>
   );
 }
 
